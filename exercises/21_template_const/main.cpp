08aae05a759ee0bbfc949345968afd934f3f2df7
TenHian <tenhian.cn@gmail.com>
2024-08-04 18:54:28 +0800

    14, 19-29

--- exercises/21_template_const/main.cpp
+++ exercises/21_template_const/main.cpp
@@ -1,5 +1,6 @@
-﻿#include "../exercise.h"
+#include "../exercise.h"
 #include <cstring>
+#include <numeric>
 
 // READ: 模板非类型实参 <https://zh.cppreference.com/w/cpp/language/template_parameters#%E6%A8%A1%E6%9D%BF%E9%9D%9E%E7%B1%BB%E5%9E%8B%E5%AE%9E%E5%8F%82>
 
@@ -11,6 +12,8 @@ struct Tensor {
     Tensor(unsigned int const shape_[N]) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+        memcpy(shape, shape_, sizeof(int) * N);
+        size = std::accumulate(std::begin(shape), std::end(shape), 1, std::multiplies<int>());
         data = new T[size];
         std::memset(data, 0, size * sizeof(T));
     }
@@ -35,6 +38,11 @@ private:
         for (unsigned int i = 0; i < N; ++i) {
             ASSERT(indices[i] < shape[i], "Invalid index");
             // TODO: 计算 index
+            int steps = 1;
+            for (unsigned int j = i + 1; j < N; ++j) {
+                steps *= shape[j];
+            }
+            index += indices[i] * steps;
         }
         return index;
     }
--- exercises/25_strides/main.cpp
+++ exercises/25_strides/main.cpp
@@ -1,4 +1,4 @@
-﻿#include "../exercise.h"
+#include "../exercise.h"
 #include <vector>
 
 // 张量即多维数组。连续存储张量即逻辑结构与存储结构一致的张量。
@@ -18,6 +18,14 @@ std::vector<udim> strides(std::vector<udim> const &shape) {
     // TODO: 完成函数体，根据张量形状计算张量连续存储时的步长。
     // READ: 逆向迭代器 std::vector::rbegin <https://zh.cppreference.com/w/cpp/container/vector/rbegin>
     //       使用逆向迭代器可能可以简化代码
+    if (!shape.empty()) {
+        strides.back() = 1;
+    }
+
+    auto iter = shape.rbegin();
+    for (auto rit = strides.rbegin() + 1; rit != strides.rend(); ++rit, ++iter) {
+        *rit = *(rit - 1) * (*iter);
+    }
     return strides;
 }
 
